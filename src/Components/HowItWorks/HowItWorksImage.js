bdaa2cf73d094f022c1588eb66c8cedfe6b00dde
Andrii <andriikostiv@gmail.com>
2019-05-02 17:51:25 +0300

    Created responsive HowItWorks Component

---
+++ src/Components/HowItWorks/HowItWorksImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import ArticleIMG from '../../images/ArticleIMG.png';
+
+const HowItWorksImage = () => (
+    <section className = "HowItWorksImageBox">
+        <img className = "HowItWorksImage" src = {ArticleIMG} alt = " "/>
+    </section>
+);
+
+export default HowItWorksImage;
