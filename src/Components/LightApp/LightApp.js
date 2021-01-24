a25ac7a87a86858bb804e7bfd54c0e81835be70c
Andrii <costivandrei@gmail.com>
2021-01-24 21:15:08 +0000

    removed gh-pages and connected all components back

--- src/Components/LightApp/LightApp.js
+++ src/Components/LightApp/LightApp.js
@@ -5,7 +5,7 @@ import HeaderComponent from "../HeaderComponent/HeaderComponent";
 import OurAdvantages from "../OurAdvantages/OurAdvantages";
 import HowItWorks from "../HowItWorks/HowItWorks";
 import MiningPlan from "../MiningPlan/MiningPlan";
-// import ClientsReviews from "../ClientsReviews/ClientsReviews"
+import ClientsReviews from "../ClientsReviews/ClientsReviews"
 import ContactForm from "../ContactForm/ContactForm";
 
 const LightApp = ({lightTheme}) => {  
@@ -21,7 +21,7 @@ const LightApp = ({lightTheme}) => {
 
                 <MiningPlan/>
 
-                {/* <ClientsReviews /> */}
+                <ClientsReviews />
 
                 <ContactForm/>
             </section>
